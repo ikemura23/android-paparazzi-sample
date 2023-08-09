b8a2fad59b12cae69893f87db838ef3bd7d7728b
ikemura23 <banbara.work@gmail.com>
2023-08-09 18:01:16 +0900

    feature: Tagコンポーネントを作成して表示

---
+++ app/src/main/java/com/ikemura23/paparazzi/sample/ui/component/Tag.kt
@@ -0,0 +1,39 @@
+package com.ikemura23.paparazzi.sample.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.contentColorFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Tag(
+    name: String,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = MaterialTheme.colorScheme.background,
+    contentColor: Color = contentColorFor(backgroundColor),
+) {
+    Text(
+        text = "#$name".lowercase(),
+        modifier = modifier
+            .background(
+                color = backgroundColor,
+                shape = MaterialTheme.shapes.small,
+            )
+            .padding(horizontal = 4.dp, vertical = 2.dp),
+        color = contentColor,
+        fontSize = 12.sp,
+    )
+}
+
+@Preview
+@Composable
+private fun TagPreview() {
+    Tag("preview")
+}
